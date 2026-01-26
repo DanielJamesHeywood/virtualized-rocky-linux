4453072cbb8c8c718d7801d9eb41e514cdf4e5f5
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-26 17:54:12 +0000

    Initial Commit

---
+++ virtualized rocky linux/virtualized_rocky_linuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtualized_rocky_linuxApp.swift
+//  virtualized rocky linux
+//
+//  Created by Daniel James Heywood on 26/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct virtualized_rocky_linuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
