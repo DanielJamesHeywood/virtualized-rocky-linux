636ea26d5c784d8891e23c35e327945e8fd101e7
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-26 21:30:44 +0000

    Fixed declaration indentation.

--- virtualized rocky linux/VirtualizedRockyLinuxApp.swift
+++ virtualized rocky linux/VirtualizedRockyLinuxApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct VirtualizedRockyLinuxApp: App {
+    
     var body: some Scene {
         Window("virtualized rocky linux", id: "virtualizedRockyLinuxApp", content: { ContentView() })
     }
