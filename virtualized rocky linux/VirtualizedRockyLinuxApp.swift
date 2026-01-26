65e9e12b8c8f5b82539ab24b27c7f47f644f8226
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-26 21:44:28 +0000

    Moved virtual machine initialization from the app to its associated view.

--- virtualized rocky linux/VirtualizedRockyLinuxApp.swift
+++ virtualized rocky linux/VirtualizedRockyLinuxApp.swift
@@ -1,22 +1,9 @@
 import SwiftUI
-import Virtualization
 
 @main
 struct VirtualizedRockyLinuxApp: App {
     
-    let _virtualMachine = _makeVirtualMachine()
-    
     var body: some Scene {
         Window("virtualized rocky linux", id: "virtualizedRockyLinuxApp", content: { VirtualizedRockyLinuxView() })
     }
-}
-
-func _makeVirtualMachine() -> VZVirtualMachine {
-    let virtualMachine = VZVirtualMachine(configuration: _makeVirtualMachineConfiguration())
-    return virtualMachine
-}
-
-func _makeVirtualMachineConfiguration() -> VZVirtualMachineConfiguration {
-    let configuration = VZVirtualMachineConfiguration()
-    return configuration
 }
--- virtualized rocky linux/VirtualizedRockyLinuxView.swift
+++ virtualized rocky linux/VirtualizedRockyLinuxView.swift
@@ -4,8 +4,21 @@ import Virtualization
 struct VirtualizedRockyLinuxView: NSViewRepresentable {
     
     func makeNSView(context: Context) -> VZVirtualMachineView {
-        VZVirtualMachineView()
+        let view = VZVirtualMachineView()
+        view.virtualMachine = _makeVirtualMachine()
+        return view
     }
     
     func updateNSView(_ view: VZVirtualMachineView, context: Context) {}
 }
+
+func _makeVirtualMachine() -> VZVirtualMachine {
+    let virtualMachine = VZVirtualMachine(configuration: _makeVirtualMachineConfiguration())
+    return virtualMachine
+}
+
+func _makeVirtualMachineConfiguration() -> VZVirtualMachineConfiguration {
+    let configuration = VZVirtualMachineConfiguration()
+    return configuration
+}
+
