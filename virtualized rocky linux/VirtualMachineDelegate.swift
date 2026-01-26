322252b58433eb9faa9486c163d1bd3fb2678ade
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-26 22:20:57 +0000

    Terminate application when guest stops.

--- virtualized rocky linux/VirtualMachineDelegate.swift
+++ virtualized rocky linux/VirtualMachineDelegate.swift
@@ -2,7 +2,9 @@ import Virtualization
 
 class VirtualMachineDelegate: NSObject, VZVirtualMachineDelegate {
     
-    func guestDidStop(_ virtualMachine: VZVirtualMachine) {}
+    func guestDidStop(_ virtualMachine: VZVirtualMachine) {
+        NSApplication.shared.terminate(nil)
+    }
     
     func virtualMachine(_ virtualMachine: VZVirtualMachine, didStopWithError error: any Error) {
         fatalError("Virtual machine stopped with error: \(error)")
