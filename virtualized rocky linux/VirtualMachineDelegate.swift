efa744cc75d2200f5bac2e93e059114f858e0336
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-26 22:22:38 +0000

    Extracted termination logic to a utility function.

--- virtualized rocky linux/VirtualMachineDelegate.swift
+++ virtualized rocky linux/VirtualMachineDelegate.swift
@@ -3,10 +3,14 @@ import Virtualization
 class VirtualMachineDelegate: NSObject, VZVirtualMachineDelegate {
     
     func guestDidStop(_ virtualMachine: VZVirtualMachine) {
-        NSApplication.shared.terminate(nil)
+        terminate()
     }
     
     func virtualMachine(_ virtualMachine: VZVirtualMachine, didStopWithError error: any Error) {
         fatalError("Virtual machine stopped with error: \(error)")
     }
+}
+
+func terminate() {
+    NSApplication.shared.terminate(nil)
 }
