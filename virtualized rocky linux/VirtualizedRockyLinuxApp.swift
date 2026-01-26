756d6470859affb205fe39eb5bb56a79c5bb64b9
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-26 19:57:31 +0000

    Improvements to capitalization of the main app.

--- virtualized rocky linux/VirtualizedRockyLinuxApp.swift
+++ virtualized rocky linux/VirtualizedRockyLinuxApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct virtualized_rocky_linuxApp: App {
+struct VirtualizedRockyLinuxApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
