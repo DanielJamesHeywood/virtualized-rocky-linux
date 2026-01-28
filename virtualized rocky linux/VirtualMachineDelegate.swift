06e688d01745a25a033ef29e56e3863e7650170c
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-28 02:56:24 +0000

    Removed function wrappers because we have loads of them and wrapping literally one function is a waste of time.

--- virtualized rocky linux/VirtualMachineDelegate.swift
+++ virtualized rocky linux/VirtualMachineDelegate.swift
@@ -3,14 +3,10 @@ import Virtualization
 class VirtualMachineDelegate: NSObject, VZVirtualMachineDelegate {
     
     func guestDidStop(_ virtualMachine: VZVirtualMachine) {
-        terminate()
+        NSApplication.shared.terminate(nil)
     }
     
     func virtualMachine(_ virtualMachine: VZVirtualMachine, didStopWithError error: any Error) {
         fatalError("Virtual machine stopped with error: \(error)")
     }
-}
-
-func terminate() {
-    NSApplication.shared.terminate(nil)
 }
